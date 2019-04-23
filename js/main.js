e491902b9bc0ce0f49da624fad22b2501fadd090
Bartosz Oficjalski <oficjalski90@gmail.com>
2019-04-23 16:23:24 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+var isPremiumUser = false;
+
+if (isPremiumUser == true) {
+    console.log("thanks for being loyal customer")
+}else{
+    console.log("subscribe our premium service")
+}
+
+var myAge = 28;
+
+if (myAge < 1){
+    console.log("You are a baby")
+}else if(myAge < 3){
+    console.log("You are a toddler")
+}else if(myAge < 10){
+    console.log("You are a big kid")
+}else if(myAge <= 19){
+    console.log("You are a teenager")
+}else if(myAge < 40){
+    console.log("You are an adult")
+}else {
+    console.log("out of our records")
+}
